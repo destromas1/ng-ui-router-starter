92a268388f8f4e7660508f204762149e02940432
Shahjada <shahjada.talukdar@gmail.com>
2016-08-05 23:15:09 +0600

    dashboard

---
+++ app/dashboard-controller.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+angular.module("app").controller('dashboardCtrl', ['$scope','$state','$stateParams', function ($scope , $state, $stateParams) {
+    console.log('dashboardCtrl is running...');
+
+    $scope.passNameToProfile = function () {
+        $state.go('app.url', { url: 'profile' ,data: { dashboardName: $scope.dashboardName } });
+    };
+
+    $scope.currencyVal = undefined;
+    $scope.phonenoVal = undefined;
+
+}]);
+
