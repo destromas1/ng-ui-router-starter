96e494f3e482e771324763706371fae8286a273b
Shahjada <shahjada.talukdar@gmail.com>
2016-08-05 23:32:37 +0600

    Contact

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ng-ui-router-starter
 ng-ui-router-starter
+npm install
+bower install
+gulp
+
+Browse http://localhost:9001/#/
---
+++ app/contact-controller.js
@@ -0,0 +1,24 @@
+﻿"use strict";
+
+
+angular.module("app").controller('contactCtrl', ['$scope','$state','$stateParams', function ($scope , $state, $stateParams) {
+
+    console.log('contactCtrl is running...');
+
+    $scope.num1 = 100;
+    $scope.num2 = 100;
+    $scope.num3 = 100;
+    $scope.sum = $scope.num1 + $scope.num2 + $scope.num3;
+
+    $scope.changeNumber = function () {
+      $scope.sum = $scope.num1 + $scope.num2 + $scope.num3;
+    };
+
+    $scope.changeSum = function () {
+      var quarterOfNewSum = $scope.sum / 4;
+      $scope.num1 = quarterOfNewSum * 2;
+      $scope.num2 = $scope.num3 = quarterOfNewSum;
+    };
+
+}]);
+
