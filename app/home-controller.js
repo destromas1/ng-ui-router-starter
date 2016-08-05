d2a3cb5a9db28df63eaa2f2b3d7f1adb46351530
Shahjada <shahjada.talukdar@gmail.com>
2016-08-06 00:13:34 +0600

    home-controller.js

---
+++ app/home-controller.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+angular.module("app").controller('homeCtrl', ['$scope','$state', function ($scope , $state) {
+    console.log('homeCtrl is running...');
+
+    $scope.redirectRoute = function (routeName) {
+        //alert(routeName);
+        $state.go('app.url' , {url : routeName});
+    };
+
+}]);
+
